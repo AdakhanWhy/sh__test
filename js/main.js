97b44288557acd6052c296a1699923d522e73d1e
AdakhanWhy <openpy@mail.ru>
2023-05-03 12:30:34 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,89 @@
+// function sayHello(text) {
+//     let newText = '';
+//     for(let i = text.length - 1; i >= 0; --i){
+//         newText += text[i]
+//     }
+//     return newText
+// }
+
+// console.log(sayHello('12345'))
+
+// console.log(new CountQueuingStrategy(1))
+
+// function one (one) {
+//     two(2)
+//     console.log(one)
+// }
+
+// function two (two) {
+//     one(1)
+//     console.log(two)
+// }
+
+// one(1)
+
+// function one(){
+//     let count = 0
+//     return function two(){
+//         return count += 1;
+//     }
+// }
+
+// let oneVar = one()
+// oneVar()
+// oneVar()
+// oneVar()
+
+// let twoVar = one()
+// twoVar()
+// twoVar()
+
+// console.log(oneVar(), twoVar())
+
+
+// function reverse(text){
+    // let str = '';
+    // text = text.toString().split('').reverse().join('');
+    // let newText = text.toString().split('').map(item => item + ' + ').join('')
+    // newText = newText.slice(0, newText.length -2)
+    // text && console.log(true)
+    // console.log(newText)
+// }
+// reverse(1)
+
+// let a = [15];
+// console.log(a)
+// let b = a;
+// console.log(b)
+// a[0] = 151
+// console.log(a)
+// console.log(b)
+
+// let [k, n] = [3,Number(prompt())]
+// console.log(k, n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
